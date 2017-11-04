06713465904b1283bad88a0e9892b1891ccb8619
Mark Fruman <mark.fruman@yahoo.com>
2017-11-04 14:45:35 -0400

    server.py now reads excel file into pandas dataframe

--- Server/srv519/srv519/server.py
+++ Server/srv519/srv519/server.py
@@ -1,15 +1,35 @@
 # all the imports
 from flask import Flask, request, session, render_template, url_for, json
+from werkzeug.utils import secure_filename
+
+import sys
+import os.path
+import pandas as pd
+
+UPLOAD_FOLDER = '.'
+ALLOWED_EXTENSIONS = set(['xls', 'xlsx', 'csv'])
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , server.py
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return ('.' in filename
+            and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
 
 #---ROUTES----
-@app.route("/")
-def movie_rater():
-    # session['user_choices'] = list(np.zeros(len(content_mat)))
-    # initialize random y-coordinate:
-    # session['ratings_y'] = list(2.0*np.random.sample(len(content_mat)) - 1.0)
+@app.route("/", methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'POST':
+        print('hello!!!')
+        print(request.method)
+        #print(request)
+        #print(request.files)
+        print("It's me!!!")
+        print(request.files.to_dict())
+        print(pd.read_excel(request.files['excel']))
+
+    print(request.method)
     return render_template('client.html')
 
 if __name__ == "__main__":
